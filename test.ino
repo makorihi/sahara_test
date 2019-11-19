d64ac8cca0984dbdafddc5aeea77ecb44bf4aef5
SaharaUser <sahara@sahara.com>
2019-11-18 16:06:08 -0800

    Sahara update: Mon Nov 18 2019 16:06:09 GMT-0800 (Pacific Standard Time)

--- test.ino
+++ test.ino
@@ -3,6 +3,7 @@
 // more testing
 // blah
 // blah again
+// these comments are getting ridiculous
 void loop() {
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);               // wait for a second
