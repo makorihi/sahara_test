0ed4cb938bd9594e8eeee42149929cd8af555259
SaharaUser <sahara@sahara.com>
2019-11-20 12:27:33 -0800

    Sahara update: Wed Nov 20 2019 12:27:34 GMT-0800 (Pacific Standard Time)

--- test.ino
+++ test.ino
@@ -1,22 +1,24 @@
-// the loop routine runs over and over again forever:
-// testing
-// more testing
-// blah
-// blah again
-// these comments are getting ridiculous
-// ...
-// akjdnkjnd
-// another one
-// one more??
-// and another
-// one more
-// I can go all day
-// yup
-// indeed
-// again?
-void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
+/* 
+  Blink 
+  Turns on an LED on for one second, then off for one second, repeatedly. 
+  
+  This example code is in the public domain. 
+ */ 
+ 
+// Pin 13 has an LED connected on most Arduino boards. 
+// give it a name: 
+int led = 13; 
+
+// the setup routine runs once when you press reset: 
+void setup() { 
+  // initialize the digital pin as an output. 
+  pinMode(led, OUTPUT); 
+} 
+
+// the loop routine runs over and over again forever: 
+void loop() { 
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level) 
+  delay(1000);               // wait for a second 
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW 
+  delay(1000);               // wait for a second 
 }
