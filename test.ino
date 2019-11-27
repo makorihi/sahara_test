e121ec89a89ec55c5e514a825b7a080cdcb2e0af
SaharaUser <sahara@sahara.com>
2019-11-27 14:55:27 -0800

    Sahara update: Wed Nov 27 2019 14:55:31 GMT-0800 (Pacific Standard Time)

--- test.ino
+++ test.ino
@@ -2,13 +2,12 @@
   Blink 
   Turns on an LED on for one second, then off for one second, repeatedly. 
   
-  This example code is in the public domain. Testing
-  
-  -----
+  This example code is in the public domain. 
  */ 
  
 // Pin 13 has an LED connected on most Arduino boards. 
 // give it a name: 
+//testing
 int led = 13; 
 
 // the setup routine runs once when you press reset: 
@@ -23,4 +22,4 @@ void loop() {
   delay(1000);               // wait for a second 
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW 
   delay(1000);               // wait for a second 
-}
+} 
